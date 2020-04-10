f528efef301f2a1b3cacb933650fb559db7bcf4b
Leandro Bove <leandrobove@hotmail.com>
2020-04-09 22:30:48 -0300

    Criação de DDL SQL e listagem de convidados na View

---
+++ src/main/resources/import.sql
@@ -0,0 +1,3 @@
+insert into convidado (ID, NOME, QUANTIDADE_DE_CONVIDADOS) values (1, 'Pedro', 2);
+insert into convidado (ID, NOME, QUANTIDADE_DE_CONVIDADOS) values (2, 'Maria', 3);
+insert into convidado (ID, NOME, QUANTIDADE_DE_CONVIDADOS) values (3, 'Ricardo', 1);
